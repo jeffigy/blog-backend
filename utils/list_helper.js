9d6bbe7a7c4182557155bbcb1ba8db03901d6370
jeffigy <ejeffydev@gmail.com>
2024-04-25 23:20:51 +0800

    Part 4 e: 4.8: Blog List Tests, step 1

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined;
+  }
+
+  return blogs.reduce((prevHighest, currentBlog) => {
+    return currentBlog.likes > (prevHighest ? prevHighest.likes : 0);
+  }, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
